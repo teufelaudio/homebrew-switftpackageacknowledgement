26f2aa3fe602b0e5faa31bb3d98505bd8839e311
Thomas Mellenthin <thomas.mellenthin@teufel.de>
2022-08-09 13:32:11 +0200

    Homebrew formula

---
+++ Formula/spm-ack.rb
@@ -0,0 +1,17 @@
+class Xcparse < Formula
+    desc "Create Licensing Views from SwiftPackageManager Packages"
+    homepage "https://github.com/teufelaudio/SwiftPackageAcknowledgement"
+    url "https://github.com/teufelaudio/SwiftPackageAcknowledgement.git", :tag => "0.1.6"
+    head "https://github.com/teufelaudio/SwiftPackageAcknowledgement.git"
+  
+    depends_on :xcode => ["11.0", :build]
+  
+    def install
+      bin.mkpath
+      system "make", "install", "prefix=#{prefix}"
+    end
+  
+    test do
+      system "#{bin}/spm-ack" "-h"
+    end
+  end
